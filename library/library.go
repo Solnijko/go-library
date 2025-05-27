ca266aaa2aedc0687787878255935cfc3fc9ab5a
Moskaluk Maksim <m.moskaluk@minervasoft.ru>
2025-05-27 21:11:59 +0300

    Remade search by title function, deleted excessive console outputs

--- library/library.go
+++ library/library.go
@@ -19,7 +19,6 @@ func AddBook(newTitle string, newDescription string, isRead bool) {
 		isRead:      isRead,
 	}
 	Books = append(Books, newBook)
-	fmt.Printf("Book '%s' was added to the library\n", Books[BookId].title)
 	BookId += 1
 }
 
@@ -47,10 +46,12 @@ func FindBookById(book_id int) (*Book, error) {
 }
 
 func FindBookByTitle(book_title string) (*Book, error) {
+	var booksByTitle = make(map[string]*Book)
 	for _, book := range Books {
-		if book.title == book_title {
-			return book, nil
-		}
+		booksByTitle[book.title] = book
+	}
+	if book, exists := booksByTitle[book_title]; exists {
+		return book, nil
 	}
 	return nil, fmt.Errorf("Book with such title was not found")
 }
